787cd2cd8be7b05443d8ab15489f8e5150875ce8
Thomas Girgenti <thomasg@disroot.org>
2019-05-20 10:49:08 -0600

    create production build

---
+++ build/precache-manifest.393380f750010efa7a8d216a9ec5b9b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3351b12035d29d28cdcf251c3a039cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "708e771fe1fa594fba8d",
+    "url": "/static/css/main.b9a414ac.chunk.css"
+  },
+  {
+    "revision": "770bb6e19da09fde4929",
+    "url": "/static/js/2.a1351bd8.chunk.js"
+  },
+  {
+    "revision": "708e771fe1fa594fba8d",
+    "url": "/static/js/main.4ed186c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
